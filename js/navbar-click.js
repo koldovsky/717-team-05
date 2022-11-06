f27738bf3cec9d27e2b81b5cf6730d99d6b33c63
Levko Tkach <91378152+LevkoTkach@users.noreply.github.com>
2022-11-06 16:43:55 +0200

    add js click

--- js/navbar-click.js
+++ js/navbar-click.js
@@ -1,7 +1,13 @@
 (function(){
-    const navigatinLink = document.querySelectorAll('a');
-    function close () {
-        onclick.navigatinLink = 
-    }
-
+    const navBurgetrList = document.querySelector('#navburger');
+    const navigatinLinks = document.querySelectorAll('.header__nav__item>a');
+    navigatinLinks.forEach((navLink) => {
+        navLink.addEventListener("click", (_) => {
+            if(navBurgetrList.classList.contains('show')){
+              document.body.classList.remove("overflow: hidden; padding-right: 17px;");
+              navBurgetrList.classList.remove("show");
+            }
+        })
+     }
+    );
 })()
